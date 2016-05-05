739881c658307affac1f8a3dcd6789bae47c9410
Simon Pettersson <simon.pettersson@gmail.com>
2016-05-06 01:26:10 +0200

    Added pouch-redux-middleware

--- src/store/database.js
+++ src/store/database.js
@@ -1,35 +1,59 @@
 import PouchDb from 'pouchdb'
+import PouchMiddleware from 'pouch-redux-middleware'
 PouchDb.plugin(require('pouchdb-authentication'))
 PouchDb.debug.disable()
 
 import { AUTHENTICATION_STATE } from 'routes/Authentication/modules/authentication'
 const { AUTHENTICATED, UNAUTHENTICATED } = AUTHENTICATION_STATE
 
-const setupDatabase = (url, state) => (user) => {
+const installPouchMiddleware = (state) => {
+  state.middleware = [
+    // PouchMiddleware({
+    //   path: '/projects',
+    //   db: state.local,
+    //   actions: {
+    //   }
+    // })
+  ]
+}
+
+const setupSync = (url, state) => (user) => {
   state.local = PouchDb(user.name + '_' + user.companyId)
   state.remote = PouchDb(url + '/company_' + user.companyId)
   state.syncHandle = state.local.sync(state.remote, { live: true, retry: true })
 }
 
-const teardownDatabase = (state) => () => {
+const tearDownDb = (state) => {
+  state.syncHandle.on('complete', () => {
+    state.local.destroy() // Do not store local data when logging out!
+    delete state.local
+    delete state.remote
+    console.debug('Destroyed db')
+  })
+  state.syncHandle.cancel()
+}
+
+const tearDown = (state) => () => {
   if (state.syncHandle) {
-    state.syncHandle.on('complete', () => {
-      state.local.destroy() // Do not store local data when logging out!
-      delete state.local
-      delete state.remote
-      console.debug('Destroyed db')
-    })
-    state.syncHandle.cancel()
+    tearDownDb(state)
   }
 }
 
 export const createCompanyDbMiddleware = (url, dispatch) => {
 
-  let state = {}
+  let state = {
+    local: undefined,
+    remote: undefined,
+    syncHandle: undefined,
+    middleware: []
+  }
 
   const actions = {
-    [AUTHENTICATED]: setupDatabase(url, state),
-    [UNAUTHENTICATED]: teardownDatabase(state)
+    [AUTHENTICATED]: () =>  {
+      setupSync(url, state)
+      installPouchMiddleware(state)
+    },
+    [UNAUTHENTICATED]: tearDown(state)
   }
 
   return (store) => (next) => (action) => {
@@ -38,6 +62,7 @@ export const createCompanyDbMiddleware = (url, dispatch) => {
     if (hookAction) {
       hookAction(payload)
     }
+    state.middleware.map((mw) => mw(store)(next)(action))
     next(action)
   }
 }
